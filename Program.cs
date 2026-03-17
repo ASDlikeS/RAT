7ee61c02e6d6655591169b73534dc5ebc898b908
ASDlikeS <penetresing_test@tutamail.com>
2026-03-17 09:53:57 +0300

    dev(CommandShell): shell console functionality

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using RAT.Modules;
 using static RAT.Menu.Menu;
+using static RAT.Shell.Shell;
 using static RAT.Utils.UtilsCollection;
 
 // var configManager = new ConfigManager();
@@ -12,4 +13,29 @@ ShowBanner();
 CheckSystem();
 
 await ModuleManager.InitializeModules();
-await MainMenuLoop();
+await Task.Delay(1000);
+InitializeCommands();
+
+Console.Write("Continue to start main menu? [Y/n] ");
+string? input = Console.ReadLine();
+bool doesContinue = true;
+while (doesContinue)
+    if (input == "n")
+    {
+        Console.WriteLine("[+] See ya later!");
+        Console.ReadKey();
+        return 0;
+    }
+    else if (input == "Y" || input == "y" || string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("[*] Starting menu...");
+        await Task.Delay(500);
+        await MainMenuLoop();
+    }
+    else
+    {
+        Console.Write("[!] Unexpected value. Choose [Y/n] ");
+        input = Console.ReadLine();
+    }
+
+return 0;
--- Shell/CommandShell.cs
+++ Shell/CommandShell.cs
@@ -1,20 +1,18 @@
-using System.Text.RegularExpressions;
 using RAT.Utils;
+using static RAT.Menu.Menu;
 
 namespace RAT.Shell;
 
 static class Shell
 {
-    private static bool isShellOpened = true;
-    private static Dictionary<string, Action<string[]>> _commands = [];
+    private static readonly Dictionary<string, Action<string[]>> _commands = [];
 
-    static Shell() { }
-
-    private static void InitializeCommands()
+    internal static void InitializeCommands()
     {
+        Console.WriteLine("[*] Initializing shell commands...");
         // Базовые команды
         _commands["help"] = (args) => ShowHelp();
-        _commands["menu"] = (args) => OpenMenu();
+        _commands["menu"] = async (args) => await OpenMenu();
         _commands["clear"] = (args) => Console.Clear();
         _commands["cls"] = (args) => Console.Clear();
         _commands["exit"] = (args) => Environment.Exit(0);
@@ -27,11 +25,12 @@ static class Shell
         // Команда config
         _commands["config"] = HandleConfigCommand;
         _commands["cfg"] = HandleConfigCommand;
+        Console.WriteLine("[+] Initializing completed");
     }
 
     internal static void TurnShell()
     {
-        while (isShellOpened)
+        while (true)
         {
             Console.Write($"[{UtilsCollection.userName}@{UtilsCollection.machineName}]$ ");
             string? input = Console.ReadLine()?.Trim();
@@ -47,18 +46,14 @@ static class Shell
 
     private static void ParseEndExecute(string input)
     {
-        var args = ParseArgument(input);
-        if (args.Count == 0)
-            return;
-
+        var args = input.Split(' ');
         string command = args[0].ToLower();
         var commandArgs = args.Skip(1).ToArray();
-
-        if (_commands.ContainsKey(command))
+        if (_commands.TryGetValue(command, out Action<string[]>? value))
         {
             try
             {
-                _commands[command](commandArgs);
+                value(commandArgs);
             }
             catch (Exception ex)
             {
@@ -73,24 +68,111 @@ static class Shell
         }
     }
 
-    private static List<string> ParseArgument(string command)
+    private static async Task OpenMenu()
     {
-        var result = new List<string>();
-        var regex = new Regex(@"[\""].+?[\""]|[^ ]+");
-
-        foreach (Match match in regex.Matches(command))
-        {
-            result.Add(match.Value.Trim('"'));
-        }
-
-        return result;
+        await MainMenuLoop();
     }
 
     private static void HandleMachineCommand(string[] args)
     {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Command [machine] requires an option. Use --help");
+            return;
+        }
+
+        string option = args[0].ToLower();
+
+        switch (option)
+        {
+            case "--list":
+            case "-l":
+                Console.WriteLine("List of machines...");
+                break;
+            case "--info":
+            case "-i":
+                if (args.Length < 2)
+                    Console.WriteLine("Usage: machine --info <id1, id2,...>");
+                else
+                    Console.WriteLine("Information about machine...");
+                break;
+            case "--rename":
+            case "-rn":
+                if (args.Length < 3)
+                    Console.WriteLine("Usage: machine --rename <id|name> <new_name>");
+                else
+                    Console.WriteLine("Rename machine...");
+                break;
+            case "--delete":
+            case "-d":
+                if (args.Length < 2)
+                    Console.WriteLine("Usage: machine --delete <id|name>");
+                else if (!UtilsCollection.isAdmin)
+                    Console.WriteLine("[!] Error: Admin right required to delete machine");
+                else
+                    Console.WriteLine("Deleting machine...");
+                break;
+            case "--open":
+            case "-o":
+                if (args.Length < 2)
+                    Console.WriteLine("Usage: machine --open <id|name>");
+                else
+                    Console.WriteLine("Opening machine...");
+                break;
+            case "--ping":
+            case "-p":
+                if (args.Length < 2)
+                    Console.WriteLine("Usage: machine --ping <all|id1,id2,...>");
+                else
+                    Console.WriteLine("Pinging machines...");
+                break;
+            case "--help":
+            case "-h":
+                ShowMachineHelp();
+                break;
+            default:
+                Console.WriteLine(
+                    $"Unknown option: {option}. Use --help to see available options."
+                );
+                break;
+        }
+    }
+
+    private static void HandleConfigCommand(string[] args)
+    {
         if (args.Length != 1)
         {
-            Console.WriteLine("Command machine must have only 1 param");
+            Console.WriteLine("Command [config] requires an option. Use --help");
+            return;
+        }
+
+        string option = args[0].ToLower();
+
+        switch (option)
+        {
+            case "--re-generate":
+            case "-rg":
+                if (!UtilsCollection.isAdmin)
+                    Console.WriteLine("[!] Error: Admin rights required to regenerate config");
+                else
+                    Console.WriteLine("Regenerating config...");
+                break;
+            case "--change-path":
+            case "-p":
+                if (args.Length < 2)
+                    Console.WriteLine("Usage: config --change-path <PATH>");
+                else
+                    Console.WriteLine("Changing path...");
+                break;
+            case "--help":
+            case "-h":
+                ShowConfigHelp();
+                break;
+            default:
+                Console.WriteLine(
+                    $"Unknown option: {option}. Use --help to see available options."
+                );
+                break;
         }
     }
 
